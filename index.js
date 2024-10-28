29328812dab1141b8a64f0480745a98042ac713a
anil royal <124272723+anildevops7702@users.noreply.github.com>
2024-10-28 11:16:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello this is kranthi_champ");
+  res.send("Hello this is Sample website");
 });
 
 app.get("/me", (req, res) => {
-  res.send("keep learning champs");
+  res.send("learning the developer tools");
 });
 
 app.listen(5000, () => {
